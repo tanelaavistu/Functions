76569238ff871e2bebae755d79e78953255c2185
Tanel Aavistu <tanel.aavistu@gmail.com>
2021-11-09 12:55:32 +0200

    first commit

---
+++ UserNameData/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UserNameData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajalt eesnime
+            //programm kuvab kasutaja eesnime pikkuse
+            //programm kuvab kasutaja eesnime esimest tähte
+            //programm kuvab kasutaja eesnime tagurpidi
+            //main meetodis ei tohi olla rohkem kui kolm rida koodi
+            Console.WriteLine("Sisesta oma eesnimi:");
+            string userName = Console.ReadLine();
+            GetUserNameData(userName);
+        }
+
+        public static void GetUserNameData(string userInput)
+        {
+            Console.WriteLine($"Sinu eesnimes on {userInput.Length} sümbolit.");
+            Console.WriteLine($"Sinu eesnime esimene täht on {userInput[0]}");
+
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+    }
+}
---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada eesnime
+            //programm küsib kasutajal sisestada numbrid 1-3
+            //kui kasutaja sisestab '1', siis kuvatakse eesnime tagurpidi
+            //kui kasutaja sisestab '2', siis kuvatakse kasutaja eesnime esimest tähte
+            //kui kasutaja sisestab '3', siis kuvatakse kasutaja eesnime pikkust
+            Console.WriteLine("Sisesta oma eesnimi:");
+            string userName = Console.ReadLine();
+            Algus:
+            Console.WriteLine("Sisesta valik 1-3:");
+            char userChoice = Convert.ToChar(Console.ReadLine());
+            switch (userChoice)
+            {
+                case '1':
+                    ResultNum(userName);
+                    break;
+                case '2':
+                    ResultFirst(userName);
+                    break;
+                case '3':
+                    ResultBack(userName);
+                    break;
+                default:
+                    Console.WriteLine("Vale väärtus!");
+                    goto Algus;
+            }
+
+
+        }
+        public static void ResultNum(string userInput)
+        {
+            Console.WriteLine($"Sinu eesnimes on {userInput.Length} sümbolit.");
+        }
+        public static void ResultFirst(string userInput)
+        {
+            Console.WriteLine($"Sinu eesnime esimene täht on {userInput[0]}");
+        }
+        public static void ResultBack(string userInput)
+        {
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+    }
+    }
